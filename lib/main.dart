60783dbfe75e71cabdda2944e8e5929aa7885efe
sdesai005-prog <sdesai005@rku.ac.in>
2026-08-27 13:03:09 +0530

    update main.dart page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'main_navigation_page.dart';
 
 void main() {
   runApp(const TeaMartApp());
@@ -11,118 +12,9 @@ class TeaMartApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-
-      home: const MyHomePage(title: 'Vasu Flutter App'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  // Increment counter
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  // Decrement counter
-  void _decrementCounter() {
-    setState(() {
-      if (_counter > 0) {
-        _counter--;
-      }
-    });
-  }
-
-  // Reset counter
-  void _resetCounter() {
-    setState(() {
-      _counter = 0;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.title,
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Counter Value',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-
-            const SizedBox(height: 20),
-
-            Text(
-              '$_counter',
-              style: const TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-            ),
-
-            const SizedBox(height: 30),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Decrement
-                ElevatedButton(
-                  onPressed: _decrementCounter,
-                  child: const Icon(Icons.remove),
-                ),
-
-                const SizedBox(width: 15),
-
-                // Reset
-                ElevatedButton(
-                  onPressed: _resetCounter,
-                  child: const Icon(Icons.refresh),
-                ),
-
-                const SizedBox(width: 15),
-
-                // Increment
-                ElevatedButton(
-                  onPressed: _incrementCounter,
-                  child: const Icon(Icons.add),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      title: 'TeaMart',
+      theme: ThemeData(scaffoldBackgroundColor: const Color(0xFFEFE2CB)),
+      home: const MainNavigationPage(),
     );
   }
 }
