4f26ace4191c1e390696749c18b06bfbbef365ab
SUJAL DESAI <sdesai005@rku.ac.in>
2026-09-10 16:06:33 +0530

    Clean up comments in widget_test.dart
    Removed commented instructions in Gujarati from the widget test.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,13 +3,12 @@ import 'package:teamart_application/main.dart';
 
 void main() {
   testWidgets('TeaMart App Screen test', (WidgetTester tester) async {
-    // App રન કરો (જો main.dart માં ક્લાસનું નામ CoffeeApp હોય તો CoffeeApp() લખવું)
+
     await tester.pumpWidget(const CoffeeApp());
 
-    // App લોડ થઈ છે કે નહીં તે ચેક કરો
+   
     expect(find.byType(CoffeeApp), findsOneWidget);
 
-    // 1 સેકન્ડ વેટ કરીને ફ્રેમ રિકોલ કરો
     await tester.pump(const Duration(seconds: 1));
   });
 }
